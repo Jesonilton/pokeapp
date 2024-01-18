df61be12c505e70c4ddaa3aaa0f4a016448330a0
jesonilton <jesonilton@gmail.com>
2024-01-18 13:07:17 -0300

    removido arquivos desnecessários

--- api/resources/js/app.js
+++ api/resources/js/app.js
@@ -1,14 +1,1 @@
 import './bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import PokemonListItem from '@/components/PokemonListItem.vue';
-
-import Vue from 'vue';
-
-new Vue({
-  render: (h) => h(PokemonListItem),
-}).$mount('#app');
-// import { defineCustomElement } from 'vue'
-
-// window.onload = function () {
-//     customElements.define('pokemon-list-item', defineCustomElement(PokemonListItem))
-// }
